50f8e35d49d9b3061a5ac1b1da145a6dc26976a4
hothorn <hothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2004-03-19 14:54:57 +0000

    best -> select.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/party@43 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- party/R/treeS4.R
+++ party/R/treeS4.R
@@ -11,8 +11,6 @@ standstat = function(W, S, cw) {
 }
 
 
-
-
 surrogates = function(v, pselect, leftw, cw, n = 1) {
 
     criterion = rep.int(0, v@p)
@@ -48,46 +46,6 @@ surrogates = function(v, pselect, leftw, cw, n = 1) {
     ss
 }
     
-best = function(v, cw) {
-    criterion = rep.int(0, v@p)
-    S = v@response@values
-    sevS = .Call("evS", S, cw)
-    Scw = S * cw
-    for (p in 1:v@p) {
-
-        svar = v@inputs[[p]]
-        tcw = cw
-        if (length(svar@whichNA) > 0) {
-            tcw[svar@whichNA] = 0
-            es = .Call("evS", S, tcw)
-            Sw = S * tcw
-        } else {
-            es = sevS
-            Sw = Scw
-        }
-        L = as.vector(svar@values %*% Sw)
-        if (class(svar) == "OrderedCategoricalVariable") {
-            ap = .Call("ec", svar@values, S, cw)
-            scores = matrix(rep(svar@scores, 
-                     length(L)/length(svar@scores)), nr = 1)
-            L = L %*% scores
-            EL = ap[[1]] %*% scores
-            VL = scores %*% ap[[2]] %*% t(scores)
-        } else { 
-            ap = .Call("evL", svar@values, S, cw, es)
-            EL = ap[[1]]
-            VL = ap[[2]]
-        } 
-        zeros = VL < v@control@varnull
-        L[zeros] = 0
-        T = L
-        T[!zeros] = ((L[!zeros] - EL[!zeros])/sqrt(VL[!zeros]))
-        criterion[p] = max(abs(T))
-    }
-    if (max(criterion) < v@control@minstat) return(NULL)
-    criterion
-}
-
 node = function(v, cw) {
 
     if (sum(cw) < v@control@minsplit) {
