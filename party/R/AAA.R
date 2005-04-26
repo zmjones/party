20c1c504c989ac7d67f2fb6ce4b08c1b17c3fbc9
hothorn <hothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2005-04-26 13:19:01 +0000

    version 0.1-5
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/party@67 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- party/R/AAA.R
+++ party/R/AAA.R
@@ -1,7 +1,7 @@
 
 # $Id$
 
-if (TRUE) {
+if (FALSE) {
 .First.lib <- function(libname, pkgname) {
     if (!require("methods")) stop("cannot load methods")
     if (!require("grid")) stop("cannot load grid")
