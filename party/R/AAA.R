a869ba60aed19fdaf21cc1de890c072779402239
hothorn <hothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2005-06-09 12:11:04 +0000

    more documentation R CMD check is happy now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/party@78 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- party/R/AAA.R
+++ party/R/AAA.R
@@ -1,18 +1,6 @@
 
 # $Id$
 
-if (FALSE) {
-.First.lib <- function(libname, pkgname) {
-    if (!require("methods")) stop("cannot load methods")
-    if (!require("grid")) stop("cannot load grid")
-    if (!require("survival")) stop("cannot load survival")
-    library.dynam("party", pkgname, libname)
-    GCtorture <<- FALSE
-    .Call("party_init", PACKAGE = "party")
-}
-}
-
-if (TRUE) {
 .onLoad <- function(lib, pkg) {
     if (!require("methods")) stop("cannot load methods")
     if (!require("grid")) stop("cannot load grid")
@@ -21,5 +9,4 @@ if (TRUE) {
     GCtorture <<- FALSE
     .Call("party_init", PACKAGE = "party")
     return(TRUE)
-}
 }
