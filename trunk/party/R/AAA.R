21d89314a992a7afcf3f053f432556ba9abda377
hothorn <hothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-02-14 18:55:44 +0000

    don't load AND attach required packages (except methods)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/party@185 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/party/R/AAA.R
+++ trunk/party/R/AAA.R
@@ -3,11 +3,6 @@
 
 .onLoad <- function(lib, pkg) {
     if (!require("methods")) stop("cannot load methods")
-    if (!require("grid")) stop("cannot load grid")
-    if (!require("survival")) stop("cannot load survival")
-    if (!require("modeltools")) stop("cannot load modeltools")
-    if (!require("strucchange")) stop("cannot load sandwich")
-    # if (!require("coin")) stop("cannot load coin")
     GCtorture <<- FALSE
     .Call("party_init", PACKAGE = "party")
     return(TRUE)
