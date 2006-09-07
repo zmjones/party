062c44a7a18cc2e902419267e6929947c1fa24ed
hothorn <hothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-09-07 11:28:44 +0000

    R-2.4.0 fixes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/party@270 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/party/R/AAA.R
+++ trunk/party/R/AAA.R
@@ -2,7 +2,6 @@
 # $Id$
 
 .onLoad <- function(lib, pkg) {
-    if (!require("methods")) stop("cannot load methods")
     GCtorture <<- FALSE
     .Call("party_init", PACKAGE = "party")
     return(TRUE)
